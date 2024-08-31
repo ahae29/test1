82f7c4e3bbd08d662c93b96bf50ecd1ec736a48a
ahae29 <82927971+ahae29@users.noreply.github.com>
2024-08-31 19:23:14 +0900

    Update s2.py

--- s2.py
+++ s2.py
@@ -4,3 +4,17 @@ st.write("dasasdfsdafsdfsadfdsfsdffsdf")
 st.write("dasasdfsdafsdfsadfdsfsdffsdf")
 st.write("dasasdfsdafsdfsadfdsfsdffsdf")
 st.write("dasasdfsdafsdfsadfdsfsdffsdf")
+import pandas as pd
+
+# 데이터 생성
+data = {
+    '열 1': ['A', 'B'],
+    '열 2': ['C', 'D'],
+    '열 3': ['E', 'F']
+}
+
+# 데이터프레임 생성
+df = pd.DataFrame(data)
+
+# 표 출력
+st.table(df)
